6e6b711f498922ccb0bde8bd7340df775da6e76d
FW1201 <132231950+FW1201@users.noreply.github.com>
2026-07-10 13:55:16 +0800

    auto: sync 2026-07-10 13:55

---
+++ src/app/roster-tools/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/layout/AppShell";
+import { RosterToolsView } from "@/components/rosters/RosterToolsView";
+
+export default function RosterToolsPage() {
+  return (
+    <AppShell>
+      <RosterToolsView />
+    </AppShell>
+  );
+}
